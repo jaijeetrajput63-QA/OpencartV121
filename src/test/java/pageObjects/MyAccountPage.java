c3f79d45e9765d93a4840a1f4319a042e3730af8
jaijeetrajput63-QA <jaijeetrajput63@gmail.com>
2026-05-31 17:59:31 +0530

    first commit

---
+++ src/test/java/pageObjects/MyAccountPage.java
@@ -0,0 +1,36 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MyAccountPage extends BasePage {
+	
+	public MyAccountPage(WebDriver driver) {
+		super (driver);
+	}
+
+	@FindBy(xpath = "//h2[text()='My Account']") // MyAccount Page heading 
+	WebElement msgHeading;
+	
+	@FindBy(xpath = "//a[@class='list-group-item'][normalize-space()='Logout']") // Added in step6
+	WebElement lnkLogout;
+	
+	public boolean isMyAccountPageExist()
+	{
+		try
+		{
+		return (msgHeading.isDisplayed());
+		}
+		catch(Exception e)
+		{
+			return false; 
+		}
+	}
+	
+	public void clickLogout()
+	{
+		lnkLogout.click();
+	}
+
+}
---
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -0,0 +1,67 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC001_AccountRegistrationTest extends BaseClass {
+
+	@Test(groups={"Regression","Master"})
+	public void verify_account_registration()
+	{
+		logger.info("*** Starting TC001_AccountRegistrationTest ***");
+		
+		try
+		{
+	    HomePage hp=new HomePage(driver);
+	    hp.clickMyAccount();
+		logger.info("Clicked on MyAccount link ");
+		
+	    hp.clickRegister();
+	    logger.info("Clicked on Register link ");
+	    
+	    AccountRegistrationPage regpage=new AccountRegistrationPage(driver);
+	    
+	    logger.info("Providing customer details...");
+	    regpage.setFirstName(randomeString().toUpperCase());
+	    regpage.setLastName(randomeString().toUpperCase());
+	    regpage.setEmail(randomeString()+"@gmail.com"); // randomly generated the email-id
+	    regpage.setTelephone(randomeNumber());
+	    
+	    //String password=randomAlphaNumeric();
+	    
+	    String password=randomeAlphaNumeric();
+	    
+	    regpage.setPassword(password);
+	    regpage.setConfirmPassword(password);
+	    
+	    regpage.setPrivacyPolicy();
+	    regpage.clickContinue();
+	    
+	    logger.info("Validating expected message...");
+	    String confmsg=regpage.getConfirmationMsg();
+	    if(confmsg.equals("Your Account Has Been Created!"))
+	    {
+	    	Assert.assertTrue(true);
+	    }
+	    else
+	    {
+	    	logger.error("Test failed..");
+			logger.debug("Debug logs..");
+	    	Assert.assertTrue(false);
+	    }
+	    
+	    // Assert.assertEquals(confmsg, "Your Account Has Been Created!!!");
+		}
+		catch(Exception e)
+		{
+			 Assert.fail();
+		}
+		
+		logger.info("*** Finished TC001_AccountRegistrationTest ***");
+	}
+	
+}
